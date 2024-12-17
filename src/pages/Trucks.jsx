a36b3597813cadfa174e69f696944c6ad54cca44
OniX <gauthier.delzenne@gmail.com>
2024-12-17 12:50:07 +0100

    CORS

--- src/pages/Trucks.jsx
+++ src/pages/Trucks.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
 export default function Trucks() {
+  const [trucks, setTrucks] = React.useState([])
+
+  React.useEffect(() => {
+    fetch('https://emiwrg5h6f.execute-api.eu-central-1.amazonaws.com/prod/trucks')
+      .then(response => response.json())
+      .then(data => setTrucks(data))
+  }, [])
+
   return (
-    <div className="px-4 py-2">Trucks</div>
+    <div className="px-4 py-2 flex flex-col">
+      {trucks.map((truck, index) => (
+        <div key={index} className="p-4 my-2 bg-gray-100 rounded-md">
+          <h2 className="text-xl font-bold">Modèle : {truck.model.S}</h2>
+          <p className="text-gray-600">Capacité : {truck.capacity.N}m³</p>
+        </div>
+      ))}
+    </div>
   )
 }
